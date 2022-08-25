042884005e6d5801d58610af4a1004e3a4be4794
Kirillchumnoy <rebilion@gmail.com>
2022-08-25 13:33:18 +0300

    done_class_benchmark.py

--- tasks/easy/decorators/class_benchmark.py
+++ tasks/easy/decorators/class_benchmark.py
@@ -17,6 +17,43 @@
 """
 import time
 
-# start_time = time.time()
-# end_time = time.time()
-# difference = e - s
+start_time = time.time()
+end_time = time.time()
+difference = end_time - start_time
+
+
+def def_benchmark(func):
+    def wrapper(*args, **kwargs):
+        print(f"Выполняем {func.__name__} с args: {args} и kwargs: {kwargs}\n"
+              f"Время начала: {start_time}")
+        result = func(*args, **kwargs)
+        print(f"Выполнено {func.__name__}\n"
+              f"Время окончания: {end_time}\n"
+              f"Всего затрачено времени на выполнение: {difference}")
+        return result
+    return wrapper
+
+
+def class_benchmark(cls):
+    class_atr = {k: v for k, v in cls.__dict__.items() if k.find('_')}
+    for name, val in class_atr.items():
+        decor_metod = def_benchmark(val)
+        setattr(cls, name, decor_metod)
+    return cls
+
+
+@class_benchmark
+class Sum:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_sum(self):
+        return print(self.a + self.b)
+
+    def __repr__(self):  # Без этого метода в аргументах выводит не их значение, а ссылку на функцию
+        return f"{self.a},{self.b}"
+
+
+sum_obj = Sum(2, 3)
+sum_obj.get_sum()
